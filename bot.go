9c622f896a92f21c3c22cce7f283c08abf0cb629
kadodson612 <kdodson@nd.edu>
2019-03-02 21:29:16 -0500

    Added GIPHY stuff and linux executable

--- bot.go
+++ bot.go
@@ -5,8 +5,10 @@ import (
     "regexp"
     "strings"
     "io/ioutil"
+    "os"
 
     "github.com/nlopes/slack"
+    "github.com/peterhellberg/giphy"
 )
 
 //MAIN FUNCTION -- instantiate bot
@@ -61,9 +63,25 @@ func check_messages(text string, ev *slack.MessageEvent, rtm *slack.RTM) {
     // check if the message string exactly matches "meow"
     info := rtm.GetInfo()
     matched, _ := regexp.MatchString("^meow$", text)
+    gif_needed, _ := regexp.MatchString("^gif .*",text)
 
     if ev.User != info.User.ID && matched {
         rtm.SendMessage(rtm.NewOutgoingMessage("Is that Maru I hear?", ev.Channel))
     }
+
+    if gif_needed {
+        text = text[3:]
+        g := giphy.DefaultClient
+        res, err := g.Search([]string{text})
+        if err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+
+        if len(res.Data) > 0 {
+            rtm.SendMessage(rtm.NewOutgoingMessage(res.Data[0].BitlyGifURL, ev.Channel))
+        }
+    }
+
 
 }
